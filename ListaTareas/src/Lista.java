bac75af4c7337b3a28231b8b4d6d98353a8e6a23
Gonzalo689 <gcontrerasd01@iesalbarregas.es>
2023-09-22 00:40:25 +0200

    crear

---
+++ ListaTareas/src/Lista.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Lista {
+    private JPanel Panel;
+    private JList list;
+    private JButton borrar;
+    private JButton agregar;
+    private JTextField textField;
+
+    public JPanel getPanel() {
+        return Panel;
+    }
+}
