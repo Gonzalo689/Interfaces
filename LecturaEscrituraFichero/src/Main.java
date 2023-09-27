2a1a53b91903b0be118d35c91ade15ae9ce5fd56
Gonzalo689 <gcontrerasd01@iesalbarregas.es>
2023-09-27 21:11:19 +0200

    Escritura y lectura de ficheros

--- LecturaEscrituraFichero/src/Main.java
+++ LecturaEscrituraFichero/src/Main.java
@@ -1,8 +1,14 @@
+
+import java.awt.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-
+        LecturaEscritura lecturaEscritura = new LecturaEscritura();
+        Frame frame = lecturaEscritura.getFrame();
+        frame.setMinimumSize(new Dimension(500, 300));
+        frame.pack();
+        frame.setVisible(true);
     }
 }
