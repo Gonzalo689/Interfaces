efb99923fde300666d1f05be571020a6e009e48a
Gonzalo689 <gcontrerasd01@iesalbarregas.es>
2023-09-29 20:26:21 +0200

    Escritura y lectura de ficheros

--- Preferencias/src/Main.java
+++ Preferencias/src/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Preferencias");
+        Preferencias gestionDeContactos = new Preferencias();
+        frame.getContentPane().add(gestionDeContactos.getPanel());
+        frame.setMinimumSize(new Dimension(350, 250));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- Preferencias/src/Preferencias.java
+++ Preferencias/src/Preferencias.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class Preferencias {
+    private JPanel panel;
+    private JTextField textField1;
+    private JRadioButton opcion1RadioButton;
+    private JCheckBox checkBox1;
+    private JRadioButton opcion2RadioButton;
+    private JButton button1;
+
+    public JPanel getPanel() {
+        return panel;
+    }
+    public Preferencias(){
+
+    }
 }
