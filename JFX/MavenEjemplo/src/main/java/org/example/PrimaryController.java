299362d44e675a2c8f03f8a0499accd43b31b795
Gonzalo689 <gcontrerasd01@iesalbarregas.es>
2023-10-17 17:48:33 +0200

    .

--- JFX/MavenEjemplo/src/main/java/org/example/PrimaryController.java
+++ JFX/MavenEjemplo/src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,82 @@
 package org.example;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import com.opencsv.CSVWriter;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 
 public class PrimaryController {
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtTelefono;
+    @FXML
+    private ListView tablaIntereses;
+    @FXML
+    private DatePicker dpFechaNacimiento;
+
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void initialize(){
+        tablaIntereses.getItems().addAll("Deportes", "Música", "Viajes", "Cine", "Libros");
+
+        dpFechaNacimiento.setConverter(new StringConverter<>() {
+            final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+
+
+    }
+
+    @FXML
+    private void guardar(){
+        String fecha = String.valueOf(dpFechaNacimiento.getValue());
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter("contactos.csv", true))) {
+
+            String[] datos = {txtNombre.getText(), txtEmail.getText(), txtTelefono.getText(),
+                    fecha,  tablaIntereses.getSelectionModel().getSelectedItem().toString()};
+            csvWriter.writeNext(datos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        limpiarControles();
+    }
+    private void limpiarControles(){
+        txtNombre.clear();
+        txtEmail.clear();
+        txtTelefono.clear();
+        dpFechaNacimiento.setValue(null);
+        tablaIntereses.getSelectionModel().clearSelection();
+    }
+    @FXML
+    private void cancelar(){
+        Platform.exit();
     }
 }
--- LecturaEscrituraArchivos/src/main/java/module-info.java
+++ LecturaEscrituraArchivos/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.opencsv;
 
     opens org.example to javafx.fxml;
     exports org.example;
